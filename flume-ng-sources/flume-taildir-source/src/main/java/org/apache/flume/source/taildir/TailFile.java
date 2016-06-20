39307e7e56e88d6eb6ac19b9690f16c7acd48576
Mike Percy <mpercy@cloudera.com>
2016-06-20 01:09:07 -0700

    FLUME-2918. Speed up TaildirSource on directories with many files
    This patch greatly improves the performance of TaildirSource on
    directories that contain a large number of files.
    
    (Attila Simon via Mike Percy)
    
    Change-Id: Ifc19657e3585cde0d857c327c15dec5b2cedb75b

--- flume-ng-sources/flume-taildir-source/src/main/java/org/apache/flume/source/taildir/TailFile.java
+++ flume-ng-sources/flume-taildir-source/src/main/java/org/apache/flume/source/taildir/TailFile.java
@@ -151,13 +151,4 @@ public class TailFile {
       logger.error("Failed closing file: " + path + ", inode: " + inode, e);
     }
   }
-
-  public static class CompareByLastModifiedTime implements Comparator<File> {
-    @Override
-    public int compare(File f1, File f2) {
-      return Long.valueOf(f1.lastModified()).compareTo(f2.lastModified());
-    }
-  }
-
-
 }
